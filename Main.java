88dd472bbb449e31262d6df80cf3b3d0ccd005dd
yejinLee <yejin9989@naver.com>
2022-01-06 18:39:03 +0900

    solve

--- Main.java
+++ Main.java
@@ -6,11 +6,7 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    static int sum = 0;
-
-    static int[] num = {3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10};
-
     public static void main(String[] args) throws IOException{
-        P5622.run(args);
+        P2941.run(args);
     }
 }
---
+++ P2941.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class P2941 {
+    // https://www.acmicpc.net/problem/5622
+
+    // 입출력을 위한 변수 br, bw
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    // 크로아티아 알파벳 대체 문자열 배열
+    static String[] croatia = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+    // 실행 할 메소드
+    public static void run(String[] args) throws IOException {
+        // 입력받아서 input에 저장
+        String input = br.readLine();
+
+        // 크로아티아 알파벳에 해당하는 문자들을 다 "1 "(1과 공백)으로 치환해준다.
+        // 정규표현식 활용
+        for (int i = 0; i < croatia.length; i++) {
+            input = input.replaceAll(croatia[i], "1 ");
+        }
+
+        // 크로아티아 알파벳을 제외하면 그냥 하나씩이다
+        input = input.replaceAll("[a-z]", "1 ");
+
+        // 공백으로 구분
+        String[] token = input.split("\\p{Space}");
+
+        // 토큰 길이 출력
+        bw.write(String.valueOf(token.length));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
